ec2b11163858d06f45f05b064c62dd533f6cdeb4
PrinceVhelbreiBarbarona <princezyian.barbarona@smc.pshs.edu.ph>
2026-01-13 13:30:00 +0800

    Add files via upload
    Did FA 2 and uploaded the file to github

---
+++ Barbarona_Q3_FA02.py
@@ -0,0 +1,10 @@
+arr = [[4500, 5200, 4800, 5000, 5300],
+       [4000, 4100, 3900, 4200, 4600],
+       [6000, 5800, 5900, 6100, 6200]]
+m = arr[0]
+ju = arr[2][2]
+print("Jake's steps on wednesday:", ju)
+print("My steps...", m)
+print("Updating steps on thursday to 5500.")
+arr[0][3] = 5500
+print("Updated steps...", m)
