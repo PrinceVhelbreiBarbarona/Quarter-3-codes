e319decc3d2614c4bdc3f8c88bf3a3df0d3fac91
PrinceVhelbreiBarbarona <princezyian.barbarona@smc.pshs.edu.ph>
2026-01-20 10:41:38 +0800

    Add files via upload
    Did FA6

---
+++ Barbarona_Q3_FA6.py
@@ -0,0 +1,5 @@
+record = {}
+record["name"] = input("Enter your name: ")
+record["age"] = int(input("Enter your age: "))
+record["fav subject"] = input("Enter your favorite subject: ")
+print("Student Record:", record)
